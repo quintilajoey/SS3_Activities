c071c51bfe181aae8612a32ffcc4752eadbb1aeb
Joey Alivio Quintila <quintila.joey@ici.edu.ph>
2025-12-10 16:06:15 +0800

    Update PT2_Quintila.php

--- PT2_Quintila.php
+++ PT2_Quintila.php
@@ -3,6 +3,14 @@
     <title></title>
 </head>
 <body>
-    
+<table>
+    <tr>
+        <th>Expenditures</th>
+        <th>Estimated Cost</th>
+        <th>10% Increase</th>
+        <th>15% Increase</th>
+        <th>20% Increase</th>
+    </tr>
+</table>
 </body>
 </html>
