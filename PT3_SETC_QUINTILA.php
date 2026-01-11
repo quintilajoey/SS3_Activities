e06318d6846bcdd80c3869f196b3e8cd1085906b
Joey Alivio Quintila <quintila.joey@ici.edu.ph>
2026-01-11 16:04:09 +0800

    Update PT3_SETC_QUINTILA.php

--- PT3_SETC_QUINTILA.php
+++ PT3_SETC_QUINTILA.php
@@ -13,6 +13,10 @@
     else if ($netIncome <= 30000) {
         $tax = ($netIncome - 15000) * 0.05;
     }
+    else {
+        $tax = (15000 * 0.05) + (($netIncome - 30000) * 0.10);
+    }
+
 
     echo $netIncome;
 
