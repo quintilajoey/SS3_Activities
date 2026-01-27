03a5b7cee0d0ae20be9838583091b7ade25c5412
Joey Alivio Quintila <quintila.joey@ici.edu.ph>
2026-01-27 22:04:43 +0800

    Update PT4_PROBLEM3_QUINTILA.php

--- PT4_PROBLEM3_QUINTILA.php
+++ PT4_PROBLEM3_QUINTILA.php
@@ -3,6 +3,8 @@ echo "
 <style>
     body {
     font-family: Arial;
+    background-color: #025973;
+    color: white;
 }
     table {
     border-collapse: collapse;
@@ -14,8 +16,10 @@ echo "
     text-align: center;
 }
 </style>";
+
+echo "<body>";
 echo "<center>";
-echo "Prime Classification with Factor Counting";
+echo "<h2>Prime Classification with Factor Counting</h2>";
 echo "<table>";
 echo "<tr>
         <th>Number</th>
@@ -26,7 +30,7 @@ $prime = 0;
 $notPrime = 0;
 
 for ($num = 1; $num <= 20; $num++) {
-
+    
     $factors = 0;
 
     for ($i = 1; $i <= $num; $i++) {
@@ -39,19 +43,25 @@ for ($num = 1; $num <= 20; $num++) {
         echo "<tr><td>$num</td><td>Prime</td></tr>";
         $prime++;
     } else {
-        echo "<tr><td>$num</td><td>Not Prime ($factors factors)</td></tr>";
+        echo "<tr><td>$num</td><td>Not Prime $factors factors</td></tr>";
         $notPrime++;
     }
 }
-
 echo "</table>";
 
 echo "<table>";
-echo "<br>Summary<br>";
-echo "Total Prime Numbers: " . $prime . "<br>";
-echo "Total Non-Prime Numbers: " . $notPrime . "<br>";
+echo "<tr>
+        <th colspan='2'>Summary</th>
+    </tr>";
+echo "<tr>
+        <td>Total Prime Numbers</td>
+        <td>$prime</td>
+    </tr>";
+echo "<tr>
+        <td>Total Non-Prime Numbers</td>
+        <td>$notPrime</td>
+    </tr>";
 echo "</table>";
-
 echo "</center>";
+echo "</body>";
 ?>
-
