25c1fbc1aa7f8610a98b8428006c021df8f12da7
Joey Alivio Quintila <quintila.joey@ici.edu.ph>
2026-01-27 18:07:34 +0800

    Update PT4_PROBLEM3_QUINTILA.php

--- PT4_PROBLEM3_QUINTILA.php
+++ PT4_PROBLEM3_QUINTILA.php
@@ -1,7 +1,16 @@
 <?php
 
 for ($num = 1; $num <= 20; $num++) {
-    echo $num;
+
+    $factors = 0;
+
+    for ($i = 1; $i <= $num; $i++) {
+        if ($num % $i == 0) {
+            $factors++;
+        }
+    }
+
+    echo $num . " has " . $factors . " factors<br>";
 }
 
 ?>
