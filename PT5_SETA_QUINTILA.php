2cf5d03a36581eccee0974245483982a7e195892
Joey Alivio Quintila <quintila.joey@ici.edu.ph>
2026-01-28 09:39:36 +0800

    Update PT5_SETA_QUINTILA.php

--- PT5_SETA_QUINTILA.php
+++ PT5_SETA_QUINTILA.php
@@ -1,1 +1,19 @@
 
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>Hello, World!</title>
+</head>
+<body>
+  <form>
+    First Quarter:<input type="number"></input>
+    First Quarter:<input type="number"></input>
+    First Quarter:<input type="number"></input>
+    First Quarter:<input type="number"></input>
+
+
+
+  </form>
+</body>
+</html>
