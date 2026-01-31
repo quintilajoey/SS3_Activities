a29653e10f61799f083b8a12516ddd5b8867c101
Joey Alivio Quintila <quintila.joey@ici.edu.ph>
2026-01-31 18:58:37 +0800

    Update PT5_SETA_QUINTILA.php

--- PT5_SETA_QUINTILA.php
+++ PT5_SETA_QUINTILA.php
@@ -3,23 +3,23 @@
 
 <html>
 <head>
-  <title>Hello, World!</title>
+  <title>Grade Calculator</title>
 </head>
 <body>
-<form>
+<form method="POST">
     <fieldset>
         <legend><b>Grade Calculator</b></legend>
         <label>1st Quarter Grade:</label>
-        <input type="number" name="q1" placeholder="Enter your Grade" required><br>
+        <input type="number" name="q1" placeholder="Enter your Grade" min="70" max="99" step="0.01" required><br>
 
         <label>2nd Quarter Grade:</label>
-        <input type="number" name="q2" placeholder="Enter your Grade" required><br>
+        <input type="number" name="q2" placeholder="Enter your Grade" min="70" max="99" step="0.01" required><br>
         
         <label>3rd Quarter Grade:</label>
-        <input type="number" name="q3" placeholder="Enter your Grade" required><br>
+        <input type="number" name="q3" placeholder="Enter your Grade" min="70" max="99" step="0.01" required><br>
 
         <label>4th Quarter Grade:</label>
-        <input type="number" name="q4" placeholder="Enter your Grade" required><br>
+        <input type="number" name="q4" placeholder="Enter your Grade" min="70" max="99" step="0.01" required><br>
 
        <button type="submit" name="calculate">Calculate</button>
     </fieldset>
