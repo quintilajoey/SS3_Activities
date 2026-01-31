48e7e4d22141903d28375e7b3866e867a8eaa015
Joey Alivio Quintila <quintila.joey@ici.edu.ph>
2026-01-31 18:50:50 +0800

    Update PT5_SETA_QUINTILA.php

--- PT5_SETA_QUINTILA.php
+++ PT5_SETA_QUINTILA.php
@@ -6,14 +6,23 @@
   <title>Hello, World!</title>
 </head>
 <body>
-  <form>
-    First Quarter:<input type="number"></input>
-    First Quarter:<input type="number"></input>
-    First Quarter:<input type="number"></input>
-    First Quarter:<input type="number"></input>
+<form>
+    <fieldset>
+        <legend><b>Grade Calculator</b></legend>
+        <label>1st Quarter Grade:</label>
+        <input type="number" name="q1" placeholder="Enter your Grade" required><br>
 
+        <label>2nd Quarter Grade:</label>
+        <input type="number" name="q2" placeholder="Enter your Grade" required><br>
+        
+        <label>3rd Quarter Grade:</label>
+        <input type="number" name="q3" placeholder="Enter your Grade" required><br>
 
+        <label>4th Quarter Grade:</label>
+        <input type="number" name="q4" placeholder="Enter your Grade" required><br>
 
-  </form>
+       <button type="submit" name="calculate">Calculate</button>
+    </fieldset>
+</form>
 </body>
 </html>
