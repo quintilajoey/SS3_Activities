b1660bfd0c0712473784cbe0b8920b666babac5a
Joey Alivio Quintila <quintila.joey@ici.edu.ph>
2026-01-31 19:07:24 +0800

    Update PT5_SETA_QUINTILA.php

--- PT5_SETA_QUINTILA.php
+++ PT5_SETA_QUINTILA.php
@@ -20,9 +20,9 @@
 
         <label>4th Quarter Grade:</label>
         <input type="number" name="q4" placeholder="Enter your Grade" min="70" max="99" step="0.01" required><br>
+    </fieldset><br>
 
        <button type="submit" name="calculate">Calculate</button>
-    </fieldset>
 </form>
 </body>
 </html>
